08d674a79ef6bb645ea1517a5bf1b257687b1e9f
BionicSpoon <kingcharles678@gmail.com>
2026-05-09 17:46:33 -0500

    Add: partition-equal-subset-sum - submission-4

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-4.py
@@ -0,0 +1,27 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        # brute force: generate all possible pairs of subsets
+        # by putting each number in either subset1 or subset2
+
+        # slightly less brute force:
+        # nvm, arr could be [1, 1, 1, ..., 100]
+        # how to do dp here? Not sure really
+        # after hint: Use recursion to try to form a subset with a sum of half the total sum
+        # Then, we can return whether or not this is possible?
+        half_sum = sum(nums) / 2
+        if sum(nums) % 2:
+            return False
+        half_len = len(nums) / 2
+        memo = {}
+        def rec(i, subtotal) -> bool:
+            if i >= len(nums):
+                return subtotal == half_sum
+
+            if (i, subtotal) in memo:
+                return memo[(i, subtotal)]
+            
+            res = rec(i + 1, subtotal + nums[i]) or rec(i + 1, subtotal)
+            memo[(i, subtotal)] = res
+            return res
+
+        return rec(0, 0)
