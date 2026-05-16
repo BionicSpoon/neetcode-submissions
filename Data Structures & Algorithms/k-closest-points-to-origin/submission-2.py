a2f48f8706345e51914ff12fea84966bb72b6c4f
BionicSpoon <kingcharles678@gmail.com>
2026-05-16 16:52:04 -0500

    Add: k-closest-points-to-origin - submission-2

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        # make a heap with [(distance, [xi, yi]] as entries? This will sort by distance
+        # in O(n)? nah nlogn for heapsort'
+        # but transforming an unsorted array into heap only takes O(n)
+        # So, the sort becomes O(nlogn) because the logn comes from extracting each element in logn time
+        # so, it will be O(n * k) for sort -> get k points
+
+        q = []
+        for x, y in points:
+            q.append(((x**2 + y ** 2) ** 0.5, [x, y]))
+
+        res = []
+        heapq.heapify(q)
+        for _ in range(k):
+            point = heapq.heappop(q)[1]
+            res.append(point)
+        return res
